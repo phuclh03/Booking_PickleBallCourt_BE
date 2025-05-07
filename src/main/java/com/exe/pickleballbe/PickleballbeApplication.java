e826123ba1d2a91ff50d106177d2e6883cfa9cfa
phuclh <phuclh121003@gmail.com>
2025-05-07 13:49:23 +0700

    initial project

---
+++ src/main/java/com/exe/pickleballbe/PickleballbeApplication.java
@@ -0,0 +1,13 @@
+package com.exe.pickleballbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickleballbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickleballbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/pickleballbe/PickleballbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.pickleballbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickleballbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
